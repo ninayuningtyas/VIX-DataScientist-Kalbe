2c20842638f3eb74488437d52addafb70f61e867
Nina Ayuningtyas <76248569+ninayuningtyas@users.noreply.github.com>
2023-09-03 22:33:48 +0700

    Delete sql query directory

--- sql query/top-amount-product.sql
+++
@@ -1,7 +0,0 @@
-/*Top Total Amount from Product*/
-select t.productid as product_id, p."Product Name" as product_name, SUM(t.totalamount) as total_amount
-from "transaction" t 
-join product p 
-on t.productid = p.productid 
-group by t.productid, p."Product Name"
-order by SUM(t.totalamount) desc 
